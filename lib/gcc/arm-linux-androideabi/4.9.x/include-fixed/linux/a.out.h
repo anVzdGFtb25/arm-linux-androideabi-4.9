9730c71be3c2caba033b92e64ba069b47443312f
Rahul Chaudhry <rahulchaudhry@google.com>
2017-04-28 13:26:18 -0700

    Update prebuilt GCC to build 3956091.
    Change-Id: Ic7749dcbbe457967328f15e183ffb7a651f82110

--- lib/gcc/arm-linux-androideabi/4.9.x/include-fixed/linux/a.out.h
+++ lib/gcc/arm-linux-androideabi/4.9.x/include-fixed/linux/a.out.h
@@ -2,7 +2,7 @@
 
     It has been auto-edited by fixincludes from:
 
-	"/tmp/6f9d9bd79a1046ae3c20a17db86418d9/sysroot/usr/include/linux/a.out.h"
+	"/tmp/ed4eadb0c5251e06594cabe0af050095/sysroot/usr/include/linux/a.out.h"
 
     This had to be done to correct non-standard usages in the
     original, manufacturer supplied header file.  */
